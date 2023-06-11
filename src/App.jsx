ff080f6b3601cd5464323b5509891d5407e5406e
ocinz <ahmadhafid28632@gmail.com>
2023-06-11 18:32:46 +0700

    Develop toggle todo feature

--- src/App.jsx
+++ src/App.jsx
@@ -18,11 +18,20 @@ function App() {
       completed: false,
     },
   ]);
+  const toggleCompleted = (todoId) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === todoId) {
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
 
   return (
     <div style={styles.container}>
       <h1 style={styles.title}>My Todo List</h1>
-      <Todos todos={todos} />
+      <Todos todos={todos} toggleCompleted={toggleCompleted} />
     </div>
   );
 }
