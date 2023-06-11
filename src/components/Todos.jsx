5b025ae401d2d29e28a51d5b527ac7f9f533b9e2
ocinz <ahmadhafid28632@gmail.com>
2023-06-11 17:19:30 +0700

    create Todos and TodoItem component

---
+++ src/components/Todos.jsx
@@ -0,0 +1,12 @@
+import TodoItem from "./TodoItem";
+const Todos = ({ todos }) => {
+  return (
+    <div>
+      {todos.map((todo) => {
+        return <TodoItem key={todo.id} todo={todo} />;
+      })}
+    </div>
+  );
+};
+
+export default Todos;
